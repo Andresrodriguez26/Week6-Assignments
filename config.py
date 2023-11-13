9053a6d27bbbbe6081d4e98e3e658d8118d3ffe3
Andresrodriguez26 <anferome10@gmail.com>
2023-11-13 17:45:35 -0500

    firt commit for W6D1

---
+++ config.py
@@ -0,0 +1,14 @@
+import os 
+from dotenv import load_dotenv 
+
+basedir = os.path.abspath(os.path.dirname(__file__)) 
+
+load_dotenv(os.path.join(basedir, '.env')) 
+
+class Config():
+
+    FLASK_APP = os.environ.get('FLASK_APP') 
+    FLASK_ENV = os.environ.get('FLASK_ENV')
+    FLASK_DEBUG = True 
+    SECRET_KEY = os.environ.get('SECRET_KEY') 
+  
